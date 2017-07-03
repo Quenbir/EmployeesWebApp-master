96d1c55c3e6c37a9c35d56c6e2f02aa0c8ff6aa6
Michał Latus <michal.latus@gmail.com>
2017-07-03 20:47:07 +0200

    Add project files.

---
+++ EmployeesWebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmployeesWebApp.Models;
+
+namespace EmployeesWebApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeDbContext db = new EmployeeDbContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View("View");
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Job,Motto,Hobby,Hometown,Blog")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Job,Motto,Hobby,Hometown,Blog")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UploadPhoto(string employeeImage,
+        HttpPostedFileBase photo)
+        {
+            string path = @"E:\Projects2017\EmployeesWebApp\EmployeesWebApp\Content\Images\Employees"
+            + employeeImage;
+
+            if (photo != null)
+                photo.SaveAs(path);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePhoto(string photoFileName)
+        {
+            //Session["DeleteSuccess"] = "No";
+            var photoName = "";
+            photoName = photoFileName;
+            string fullPath = Request.MapPath("~/Images/Employees/"
+            + photoName);
+
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+                //Session["DeleteSuccess"] = "Yes";
+            }
+            return RedirectToAction("Index");
+        }
+        public PartialViewResult Select()
+        {
+            return PartialView("Select");
+        }
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EmployeesWebApp/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+namespace EmployeesWebApp.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string Motto { get; set; }
+        public string Hobby { get; set; }
+        public string Hometown { get; set; }
+        public string Blog { get; set; }
+        
+
+    }
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
